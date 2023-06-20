eed9d4c2e785f7dd466f0b0c14043f554067c4ba
vojtechdolezal <dolezal94@gmail.com>
2023-06-20 17:44:48 +0200

    Start project

---
+++ src/main/java/com/example/warhouse/WarhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.warhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warhouse/WarhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
